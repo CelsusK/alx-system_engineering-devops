ab8658d62865b6d741ac74e5a14f700297965b52
CelsusK <kiragucassian@gmail.com>
2024-08-26 05:35:53 -0700

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Install flask
+# 1-install_a_package.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
